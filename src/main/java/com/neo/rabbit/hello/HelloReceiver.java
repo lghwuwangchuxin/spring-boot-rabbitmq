35dca196ac1a8c58e868c434da46770786e7ebbb
lgh <lghwuwangchuxin@163.com>
2019-02-18 15:14:59 +0800

    Initial commit

---
+++ src/main/java/com/neo/rabbit/hello/HelloReceiver.java
@@ -0,0 +1,22 @@
+package com.neo.rabbit.hello;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 点对点发送一个队列对应一个消息
+ */
+@Component
+@RabbitListener(queues = "hello")
+public class HelloReceiver {
+
+    @RabbitHandler
+    public void process(String hello) {
+        System.out.println("Receiver  : " + hello);
+    }
+
+}
